3adb4da7cc023276942e9120f07ca02bd5d2a76d
SetupTV <96498736+ida-tv@users.noreply.github.com>
2026-05-27 20:25:17 +0300

    Refactor Firebase initialization and remove registration
    Removed user registration logic and added Firebase initialization.

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,19 +1,21 @@
-import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const auth = getAuth();
-const form = document.getElementById('reg-form');
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-eXQStkGRkueimWptlpquVyao-TBhqJk",
+  authDomain: "ida-tv-plei.firebaseapp.com",
+  projectId: "ida-tv-plei",
+  storageBucket: "ida-tv-plei.firebasestorage.app",
+  messagingSenderId: "202806935937",
+  appId: "1:202806935937:web:01cd30f5af182a47156880",
+  measurementId: "G-7R1MBNN1HH"
+};
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            sendEmailVerification(userCredential.user);
-            alert("Регистрация успешна! Подтвердите почту.");
-        })
-        .catch((error) => {
-            alert("Ошибка: " + error.message);
-        });
-});
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
