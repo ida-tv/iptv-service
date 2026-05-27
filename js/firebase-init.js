7c79ca5baa280a4b47047ecadde7c885498e2824
SetupTV <96498736+ida-tv@users.noreply.github.com>
2026-05-27 21:24:14 +0300

    Refactor Firebase initialization for authentication
    Updated Firebase imports to use CDN links and replaced analytics with authentication.

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,21 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC-eXQStkGRkueimWptlpquVyao-TBhqJk",
   authDomain: "ida-tv-plei.firebaseapp.com",
   projectId: "ida-tv-plei",
   storageBucket: "ida-tv-plei.firebasestorage.app",
   messagingSenderId: "202806935937",
-  appId: "1:202806935937:web:01cd30f5af182a47156880",
-  measurementId: "G-7R1MBNN1HH"
+  appId: "1:202806935937:web:01cd30f5af182a47156880"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
